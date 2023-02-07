eeb0a0535bac404314d88f5d228bc45afd75eadd
Andre Despouys <andredespouys@gmail.com>
2023-02-07 09:55:44 +0100

    Update 07/02. Create Intro

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,22 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  
+
   theme: {
     extend: {
       colors: {
         'primary': '#CEFF44',
+
       },
+      fontFamily: {
+        'title': ['OffBit Trial'],
+        'body': ['"Aeonik TRIAL"'],
+      }
     },
-  },
   plugins: [],
+}
 }
