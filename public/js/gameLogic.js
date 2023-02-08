3b2b96a51b1c8571fda11ea43935edbabf2735ce
Andre Despouys <andredespouys@gmail.com>
2023-02-08 11:35:51 +0100

    Update

--- public/js/gameLogic.js
+++ public/js/gameLogic.js
@@ -1,6 +1,10 @@
-function disapear () {
-  var target = document.getElementById("error1");
-    console.log("nice");
-    //element.style.display = "none";
-  }
+//This is the script that makes the game logic
 
+// Disapear elements from painting
+export function errorFound () {
+  // Make disapear the element
+  var target = document.getElementById("error");
+  target.style.display="none";
+  // Add point to score
+  //score++;
+}
--- src/components/Paint.tsx
+++ src/components/Paint.tsx
@@ -4,12 +4,11 @@ import Error from "./Error"
 
 
 export default function Paint(props:any) {
-
   return (
-    <div className="h-screen w-screen relative bg-red-200">
+    <div className="h-screen w-screen relativ">
       <div className="overflow-hidden absolute bottom-0 right-0" >
         <img className={props.class} src={props.src} alt={props.alt} />
-        <Error class="absolute w-[5.06%] z-10 top-[60.94%] left-[89.52%]" src="/paint_1_error_1.png" id="error1"/>
+        <Error class="error absolute w-[5.06%] z-10 top-[60.94%] left-[89.52%]" src="/paint_1_error_1.png" id="error1"/>
       </div>
     </div>
 
