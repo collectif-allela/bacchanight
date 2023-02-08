04bcfbb6b020bc7db8fcee43568ad675972bac42
Andre Despouys <andredespouys@gmail.com>
2023-02-08 12:12:55 +0100

    update

--- src/components/Paint.tsx
+++ src/components/Paint.tsx
@@ -8,7 +8,7 @@ export default function Paint(props:any) {
     <div className="h-screen w-screen relativ">
       <div className="overflow-hidden absolute bottom-0 right-0" >
         <img className={props.class} src={props.src} alt={props.alt} />
-        <Error class="error absolute w-[5.06%] z-10 top-[60.94%] left-[89.52%]" src="/paint_1_error_1.png" id="error1"/>
+        <Error id="error" class="absolute w-[5.06%] z-10 top-[60.94%] left-[89.52%]" src="/paint_1_error_1.png" id="error1"/>
       </div>
     </div>
 
