ef75eeb82eb942295ea022bb3a2e7bd53797a865
samuelabadie <91666297+samuelabadie@users.noreply.github.com>
2023-02-09 14:46:18 +0100

    feat: front

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -2,8 +2,7 @@
 export default function MessageBox(props) {
 
   return (
-    <div className='p-4 z-50 text-primary border-primary border-2 rounded-md bg-primary bg-opacity-20'>
-        <p className=' text-xl'>{props.text}</p>
+    <div className="w-fit bg-primary bg-opacity-20 p-4 rounded-md border-2 border-primary z-50">
         {props.children}
     </div>
   )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" className='max-w-[420px] max-h[870px] mx-auto'>
+    <Html lang="en" className='bg-white max-w-[420px] max-h[870px] mx-auto'>
       <Head />
       <body>
         <Main />
