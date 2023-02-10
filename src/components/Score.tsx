48f8d55822364c5537f903ad1829157e26ddb223
samuelabadie <91666297+samuelabadie@users.noreply.github.com>
2023-02-10 17:48:47 +0100

    feat: z-index score

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -6,6 +6,6 @@ import { useState } from "react";
 export default function Score(props:any) {
 
   return (
-    <div className="fixed bottom-5 left-5 "><span className="text-primary text-4xl " id="score">{props.score}/7</span></div>
+    <div className="fixed bottom-5 left-5 z-50"><span className="text-primary text-3xl font-offbit" id="score">{props.score}/7</span></div>
   )
 }
